879ca2699ae88578253c0ba7fc0ce7f278a316ab
hyundonk <calvin75@gmail.com>
2020-09-09 01:45:26 +0000

    Initial updates

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "rg" {
+  description = "(Required) Define the resource group name where to deploy public IP prefix"
+}
+
+variable "location" {}
+variable "tags" {}
+
+variable "nsg_rules_table" {}
+
+
